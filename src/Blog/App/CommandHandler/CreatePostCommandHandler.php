9aa153dc11c5c8e2d060cf5eacff82a9af110168
remy-hallot <remy.hallot@gmail.com>
2019-12-22 19:49:40 +0100

    Initial Commit

---
+++ src/Blog/App/CommandHandler/CreatePostCommandHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SimpleCms\Blog\App\CommandHandler;
+
+use SimpleCms\Blog\App\Command\CreatePostCommand;
+use SimpleCms\Blog\Domain\Post;
+use SimpleCms\Blog\Domain\Repository\PostWriterInterface;
+
+/**
+ * Class CreatePostCommandHandler
+ */
+class CreatePostCommandHandler
+{
+    /** @var PostWriterInterface $postWriter */
+    private $postWriter;
+
+    /**
+     * CreatePostCommandHandler constructor.
+     * @param PostWriterInterface $postWriter
+     */
+    public function __construct(PostWriterInterface $postWriter)
+    {
+        $this->postWriter = $postWriter;
+    }
+
+    /**
+     * @param CreatePostCommand $command
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function __invoke(CreatePostCommand $command): void
+    {
+        $id = $command->getId();
+        $title = $command->getTitle();
+        $content = $command->getContent();
+
+        $post = new Post($id, $title, $content);
+
+        $this->postWriter->save($post);
+    }
+}
